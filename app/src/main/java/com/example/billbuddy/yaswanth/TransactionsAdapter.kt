6d881abb92595cf0da9b0fa20735cd66f4258a36
Jing Liu <71239484+LlamaSmoothie@users.noreply.github.com>
2023-12-09 07:08:57 -0500

    UI and drawable updates

--- app/src/main/java/com/example/billbuddy/yaswanth/TransactionsAdapter.kt
+++ app/src/main/java/com/example/billbuddy/yaswanth/TransactionsAdapter.kt
@@ -5,16 +5,19 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.billbuddy.R
+import com.example.billbuddy.vinay.database.sharedpreferences.PreferenceHelper
 import com.example.billbuddy.vinay.database.transactions.TransactionEntity
+import com.example.billbuddy.vinay.views.SplitwiseApplication
 
 class TransactionsAdapter(
     private val context: Context,
     private val onTransactionClick: (TransactionEntity) -> Unit
 ) : RecyclerView.Adapter<TransactionsAdapter.ViewHolder>() {
-
+    private lateinit var preferenceHelper: PreferenceHelper
     private var transactions: List<TransactionEntity> = listOf()
     fun submitList(transactions: List<TransactionEntity>) {
         Log.d("TransactionsAdapter", "Submitting list: ${transactions.size}")
@@ -34,11 +37,21 @@ class TransactionsAdapter(
         private val textViewDescription: TextView = view.findViewById(R.id.textViewTransactionDescription)
         private val textViewAmount: TextView = view.findViewById(R.id.textViewTransactionAmount)
         private val textViewDate: TextView = view.findViewById(R.id.textViewTransactionDate)
+        private val transactionImage : ImageView = view.findViewById(R.id.transactionImage)
         fun bind(transaction: TransactionEntity) {
             textViewDescription.text = transaction.description ?: "No Description"
             textViewAmount.text = "${transaction.totalAmount}"
             textViewDate.text = transaction.transactionDateTime // Format this date as needed
             itemView.setOnClickListener { onTransactionClick(transaction) }
+            transactionImage.setImageResource(R.drawable.ic_scroll)
+            val paidBy = transaction.paidByUserId ?: -1
+            preferenceHelper = PreferenceHelper(context)
+            val userId = preferenceHelper.readLongFromPreference(SplitwiseApplication.PREF_USER_ID)
+            if (userId == paidBy){
+                transactionImage.setBackgroundResource(R.color.Dark_green)
+            }else{
+                transactionImage.setBackgroundResource(R.color.orange_splitwise)
+            }
         }
     }
 }
