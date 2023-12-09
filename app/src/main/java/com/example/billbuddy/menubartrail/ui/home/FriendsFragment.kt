33e4106c4781f49e38714ab49c04bfaaeca1ac45
Jing Liu <71239484+LlamaSmoothie@users.noreply.github.com>
2023-12-09 05:21:40 -0500

    Revert "Revert "Dashboard UI change""
    This reverts commit 7f5b3fa210b71263ce72fcbfca1e53f8a9d666ad.

--- app/src/main/java/com/example/billbuddy/menubartrail/ui/home/FriendsFragment.kt
+++ app/src/main/java/com/example/billbuddy/menubartrail/ui/home/FriendsFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
@@ -22,6 +23,7 @@ class FriendsFragment : Fragment() {
     private lateinit var rvFriends: RecyclerView
     private lateinit var tvOverallAmount: TextView
     private lateinit var btnRefresh: MaterialButton
+    private lateinit var rvFriendImage : ImageView
     private lateinit var viewModel: FriendsViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -71,6 +73,7 @@ class FriendsFragment : Fragment() {
             viewModel.updateUserId(userId)
             viewModel.refreshFriendsList()
         }
+
     }
 
     private fun getCurrentUserId(email: String, callback: (Long) -> Unit) {
--- app/src/main/java/com/example/billbuddy/yaswanth/TransactionsAdapter.kt
+++ app/src/main/java/com/example/billbuddy/yaswanth/TransactionsAdapter.kt
@@ -1,18 +1,24 @@
 package com.example.billbuddy.yaswanth
 
 import android.content.Context
+import android.graphics.Color
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.billbuddy.R
+import com.example.billbuddy.vinay.database.sharedpreferences.PreferenceHelper
 import com.example.billbuddy.vinay.database.transactions.TransactionEntity
+import com.example.billbuddy.vinay.views.SplitwiseApplication
 
 class TransactionsAdapter(
     private val context: Context,
+    private val currentUserId: Long,
     private val onTransactionClick: (TransactionEntity) -> Unit
+
 ) : RecyclerView.Adapter<TransactionsAdapter.ViewHolder>() {
 
     private var transactions: List<TransactionEntity> = listOf()
@@ -34,11 +40,23 @@ class TransactionsAdapter(
         private val textViewDescription: TextView = view.findViewById(R.id.textViewTransactionDescription)
         private val textViewAmount: TextView = view.findViewById(R.id.textViewTransactionAmount)
         private val textViewDate: TextView = view.findViewById(R.id.textViewTransactionDate)
+        private val transactionImage : ImageView = view.findViewById(R.id.transactionImage)
         fun bind(transaction: TransactionEntity) {
             textViewDescription.text = transaction.description ?: "No Description"
             textViewAmount.text = "${transaction.totalAmount}"
             textViewDate.text = transaction.transactionDateTime // Format this date as needed
             itemView.setOnClickListener { onTransactionClick(transaction) }
+            transactionImage.setImageResource(R.drawable.ic_scroll)
+            val paidBy = transaction.paidByUserId ?: -1
+            Log.e(paidBy.toString(),currentUserId.toString())
+            //preferenceHelper = PreferenceHelper(context)
+            //val userEmail = preferenceHelper.readStringFromPreference("USER_EMAIL")
+            if (currentUserId == paidBy){
+                transactionImage.setBackgroundResource(R.color.colorRed)
+            }else{
+                transactionImage.setBackgroundResource(R.color.green)
+            }
         }
+
     }
 }
